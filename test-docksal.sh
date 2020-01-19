8eb5a99609350d532e34bdbfb2dc573595fa969b
Tyler Fahey <twfahey1@gmail.com>
2020-01-18 20:51:25 -0600

    Revise test scripts to revert original composer.json

--- test-docksal.sh
+++ test-docksal.sh
@@ -8,3 +8,6 @@ vendor/bin/drush st
 echo "Testing with good-composer.json..."
 cp good-composer.json composer.json
 vendor/bin/drush st
+echo "Putting back original composer.json..."
+cp bad-composer.json composer.json
+echo "Done!"
--- test-lando.sh
+++ test-lando.sh
@@ -8,5 +8,8 @@ vendor/bin/drush st
 echo "Testing with good-composer.json..."
 cp good-composer.json composer.json
 vendor/bin/drush st
+echo "Putting back original composer.json..."
+cp bad-composer.json composer.json
+echo "Done!"
 
 EOF
